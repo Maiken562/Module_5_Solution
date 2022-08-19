47038a2d3e0c27dfa9a6335f902dfeaa4315c1b3
Maiken562 <109572237+Maiken562@users.noreply.github.com>
2022-08-18 18:10:12 -0700

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2361,19 +2361,3 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
